5357cc839d18b28dc00bdd4bead61fd47ee4d4a1
Clayton O'Neill <clayton@oneill.net>
2014-05-29 16:16:55 +0000

    Add testing for swift VIP being enabled

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,15 +1,14 @@
 require 'spec_helper'
 
 describe "openstack_lb" do 
-  let :params do 
-    { :controller_virtual_ip => '10.0.0.1',
-      :controller_state => 'MASTER',
-      :controller_names => [
-        'controller-001', 'controller-002', 'controller-003',
-      ],
-      :controller_ipaddresses => [ '10.0.0.2', '10.0.0.3', '10.0.0.4' ],
-    }
-  end
+  default_params = {
+    :controller_virtual_ip => '10.0.0.1',
+    :controller_state => 'MASTER',
+    :controller_names => [
+      'controller-001', 'controller-002', 'controller-003',
+    ],
+    :controller_ipaddresses => [ '10.0.0.2', '10.0.0.3', '10.0.0.4' ],
+  }
 
   let :facts do
     { :osfamily => 'Debian',
@@ -17,9 +16,20 @@ describe "openstack_lb" do
     }
   end
 
-  context "Without swift" do
+  context "Without swift VIP" do
+    let(:params) { default_params }
     it { should contain_sysctl__value('net.ipv4.ip_nonlocal_bind') }
     it { should contain_keepalived__instance('50') }
     it { should_not contain_keepalived__instance('51') }
+  end
+
+  context "With swift VIP" do
+    let :params do
+      { :swift_enabled => true,
+      }.merge(default_params)
+    end
+    it { should contain_sysctl__value('net.ipv4.ip_nonlocal_bind') }
+    it { should contain_keepalived__instance('50') }
+    it { should contain_keepalived__instance('51') }
   end
 end
